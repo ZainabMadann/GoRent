11d2589ad79c1c73da447ed567fde9ce8ac556d2
ZainabMadann <zainabmadann@gmail.com>
2025-03-05 16:04:02 +0300

    home page css

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("DOMContentLoaded", function () {
+    const track = document.querySelector(".image-track");
+    const images = Array.from(track.children);
+
+    images.forEach(img => {
+        let clone = img.cloneNode(true);
+        track.appendChild(clone);
+    });
+
+    let speed = 0.65; //lower = slower 
+    let position = 0;
+
+    function moveImages() {
+        position -= speed;
+        if (position <= -track.scrollWidth / 2) {
+            position = 0; 
+        }
+        track.style.transform = `translateX(${position}px)`;
+        requestAnimationFrame(moveImages);
+    }
+
+    moveImages();
+});
+
