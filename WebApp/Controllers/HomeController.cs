ed5fabea4e437984ef04702e16e491662c7f1d1e
ZainabMadann <zainabmadann@gmail.com>
2025-03-10 15:45:06 +0300

    payment page

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -47,6 +47,10 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Payment()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
