b78841a6360c0fb964c758e7208e3ba5d634b863
ProfessorMaryam <lifeasmaryam7@gmail.com>
2025-05-18 17:05:28 +0300

    small fixes

--- ClassLibrary/Migrations/20250518135529_UpdatePasswordColumnLength.Designer.cs
+++ ClassLibrary/Migrations/20250518135529_UpdatePasswordColumnLength.Designer.cs
@@ -4,6 +4,7 @@ using ClassLibrary.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClassLibrary.Migrations
 {
     [DbContext(typeof(CourseDBContext))]
-    partial class CourseDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250518135529_UpdatePasswordColumnLength")]
+    partial class UpdatePasswordColumnLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
